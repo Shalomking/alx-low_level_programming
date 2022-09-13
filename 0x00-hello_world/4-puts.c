61662520c6e0a172b7f45e0fb5c8e8d16a5c7780
Shalom Onyeukwu <favour912@gmail.com>
2022-09-12 18:59:45 -0700

    puts.c commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** Description: main - prints out a string to stdout. 
+ * Return: 0 if success.
+ */
+
+int main(viod)
+{
+	puts(*\ "programming is like building a multilingual puzzle");
+	return(0);
+}

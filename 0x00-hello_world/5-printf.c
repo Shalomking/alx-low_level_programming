061d937c2ed88c2e0acac9bca86a66f9a53a4e9c
Shalom Onyeukwu <favour912@gmail.com>
2022-09-14 19:20:02 -0700

    printf commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
-/** Description: main - prints out a string to stdout
- * Return:0 if successful
- */
+/**
+* main - print the string in the put function
+*
+* DEscription: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-

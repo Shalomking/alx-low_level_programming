52317d658217c9d5d8d64e216deb2660c07a1a82
Shalom Onyeukwu <favour912@gmail.com>
2022-09-19 16:42:13 -0700

    reset commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,21 +3,13 @@
 
 
 /**
- *
- * *reset_to_98 - resets the value of given integer to 98
- *
- * *@n: parameter
- *
- * *Return : nothing
- *
- * */
-
-
+ *reset_to_98 - resets the value of given integer to 98
+ *@n: parameter
+ *Return : nothing
+ */
 
 void reset_to_98(int *n)
 
 {
-
 		*n = 98;
-
 }

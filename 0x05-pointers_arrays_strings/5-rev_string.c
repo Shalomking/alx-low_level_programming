eed3325d5cd72121fd2418f00bb775075e84d29a
Shalom Onyeukwu <favour912@gmail.com>
2022-09-19 19:17:31 -0700

    rev string commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,75 +1,46 @@
 #include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
+ *rev_string - Reverses a string
+ *@s: string to be reversed
  *
- * * rev_string - Reverses a string
- *
- * * @s: string to be reversed
- *
- * *
- *
- * * Return: nothing
- *
- * */
-
-
+ *Return: nothing
+ */
 
 void rev_string(char *s)
-
 {
 
 		int i, tmp, len = _strlen(s);
 
+		for (i = 0; i < len / 2; i++)
+		{
 
-
-			for (i = 0; i < len / 2; i++)
-
-					{
-
-								tmp = *(s + i);
-
-										*(s + i) = *(s + len - i - 1);
-
-												*(s + len - i - 1) = tmp;
-
-													}
+		
+			tmp = *(s + i);									*(s + i) = *(s + len - i - 1);							*(s + len - i - 1) = tmp;
+		}
 
 }
 
 
 
 /**
+ *_strlen - returns the length of a string
+ *@s: string
  *
- * * _strlen - returns the length of a string
- *
- * * @s: string
- *
- * *
- *
- * * Return: the length of the given string
- *
- * */
+ *Return: the length of the given string
+ */
 
 
 
 int _strlen(char *s)
-
 {
 
-		int len = 0;
+	int len = 0;
 
+	while (*(s + len) != '\0')
+		len++;
 
-
-			while (*(s + len) != '\0')
-
-						len++;
-
-
-
-				return (len);
+	return (len);
 
 }

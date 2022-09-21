1a552e3c9d069af8554a70f917854d75ae4f05cc
Shalom Onyeukwu <favour912@gmail.com>
2022-09-21 12:31:19 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98 - take a pointer and update to 98
+ *@n: pointer 
+ *Return: void
+ */
+
+
+void reset _to_98(int *n)
+{
+	*n = 98;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void reset_to_98(int *n)
+
+
+
+
+
+{
+
+
+
+
+
+		*n = 98;
+
+
+
+
+
+}
+
+
+
+

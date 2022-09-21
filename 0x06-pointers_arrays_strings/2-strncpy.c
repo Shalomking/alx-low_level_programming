a2dd09dddd1729199f7521303ceb0fb51ccf8ae7
Shalom Onyeukwu <favour912@gmail.com>
2022-09-21 16:43:28 -0700

    strncpy commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <string.h>
 /** 
- * _strncpy -> for copying purpose
- * @dest: param1
- * @src: param2
- * @n: param3
- * Return: string
- *  
+ *_strncpy -> for copying purpose
+ *@dest: param1
+ *@src: param2
+ *@n: param3
+ *Return: string
+*/  
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
